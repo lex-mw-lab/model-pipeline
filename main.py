c87fa4905dd50d65d8b057bfac1388134558750c
Manu <mw@flexsoft.be>
2022-05-06 15:55:53 +0200

    Full run

--- main.py
+++ main.py
@@ -119,12 +119,8 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
 
-             _ =mlflow.run(
-                os.path.join(
-                    root,
-                    "src",
-                    "test_regression_model"
-                    ),
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
                 "main",
                 parameters={
                     "mlflow_model": "random_forest_export:prod",
--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
 """
-Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+long_description [An example of a step using MLflow and Weights & Biases]: Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
 import wandb
 import pandas as pd
+import os
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -14,45 +14,51 @@ logger = logging.getLogger()
 
 def go(args):
 
-    run = wandb.init(job_type="basic_cleaning")
+    run = wandb.init(job_type="job_type [my_step]: basic_cleaning")
     run.config.update(args)
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    logger.info("INFO - Download Artifact: %s", args.input_artifact)
-    data = pd.read_csv(artifact_local_path)
+    logger.info(f'Downloading artifact {args.input_artifact}')
+    artifact_local_path = run.use_artifact(args.input_artifact).file()  
 
-    min_price = args.min_price
-    max_price = args.max_price
+    # Read the data from the artifact
+    logger.info("Loading DataFrame")
+    df = pd.read_csv(artifact_local_path)
 
-    idx = data["price"].between(min_price, max_price)
+    # Remove outliers
+    logger.info('Drop outliers and convert datetime')
+    idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Remove observations outside of NYC
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
     # Save dataframe
-    output_artifact = args.output_artifact
-    output_type = args.output_type,
-    description=args.output_descriptio
-
-    logger.info(f'INFO - Saving Dataframe: {output_artifact}')
+    logger.info(f'Saving Dataframe {args.output_artifact}')
     df.to_csv('clean_sample.csv', index=False)
 
-
+    # Create an W&B artifact object
+    logger.info('Creating artifact object')
     artifact = wandb.Artifact(
-        output_artifact,
-        type=output_type,
-        description=description
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
     )
 
     artifact.add_file(local_path='clean_sample.csv')
+
+    # Upload the artifact to the run and tag it as reference
+    logger.info(f'Logging artifact {args.output_artifact}')
     run.log_artifact(artifact)
 
-    artifact.wait()
-    logger.info("INFO - Artifact uploaded.")
+    # Finish Run
+    os.remove(args.output_artifact)
+    run.finish()
 
 
 if __name__ == "__main__":
@@ -63,42 +69,42 @@ if __name__ == "__main__":
     parser.add_argument(
         "--input_artifact", 
         type=str,
-        help="Name input artifact",
+        help="Fully-qualified name for the input artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_artifact", 
         type=str,
-        help="Name output artifact",
+        help="Name for the output artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_type", 
         type=str,
-        help="Type output artifact",
+        help="Type for the output artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_description", 
         type=str,
-        help="Describe output artifact.",
+        help="Description for the output artifact",
         required=True
     )
 
     parser.add_argument(
         "--min_price", 
         type=float,
-        help="Set minimum price limit",
+        help="Minimum price for outlier range",
         required=True
     )
 
     parser.add_argument(
         "--max_price", 
         type=float,
-        help="Set maximum price limit",
+        help="Maximum price for outlier range",
         required=True
     )
 
